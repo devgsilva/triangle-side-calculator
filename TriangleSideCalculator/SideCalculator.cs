6875d2f5f16e97dc7b2368b71adebb03a3ec2806
Guilherme Silva <desenvolvedor.gsilva@gmail.com>
2025-01-21 21:28:19 -0300

    Add project files.

---
+++ TriangleSideCalculator/SideCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TriangleSideCalculator
+{
+    internal class SideCalculator
+    {
+        public double SideA;
+        public double SideB;
+        public double SideC;
+
+        public void sideCalulator(double TriangleX, double TriangleY)
+        {
+
+        }
+    }
+}
